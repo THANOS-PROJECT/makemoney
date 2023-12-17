ac97bf02cbb2cf22849a56fcbe06145e7f5891cb
saebyeokchu <cuu2252@gmail.com>
2023-12-17 21:24:00 +0900

    befor merge holiday our time file

--- analyze.py
+++ analyze.py
@@ -1,13 +1,10 @@
-import os
-from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import toTextFile
 import ourTime
+import ourConstant
 
-from pykrx import stock
 from pykrx import bond
-
 
 txts = []
 
@@ -86,7 +83,7 @@ def naverCurrentCurrency() :
     URL = ''
     page = ''
         
-def checkBond() :
+def checkNationalBond() :
     #국채 10년물의 3일치를 보고 3일치가 하락 중이라면 매도에 가중치를 준다
     #1일전 YYYYMMDD
     #3일전 YYYYMMDD
@@ -94,14 +91,44 @@ def checkBond() :
     #print(df.head())
     
     #제일큰 하락율 - 제일 작은 하락율 / 제일 큰 하락율 * 100 * 0.4 (채권 가중치)
-    print(df)
-    print(df.iloc)
-    print(df.iloc[1].수익률)
+    #print(df)
+    #print(df.iloc)
+    #print(df.iloc[1].대비)
+    증감율 = df['대비'].to_numpy()
+    최대증감율 = max(증감율, key=abs)
+    최소증감율 = min(증감율, key=abs)
+    
+    derivedValue = ( abs( 최대증감율 - 최소증감율 ) / abs(최소증감율) ) * 100 * 0.4
+    print(derivedValue)
+    
+    if derivedValue > ourConstant.__bond_standard__ :
+        print("buy somethin")
+    else :
+        print("how about wait for right timming")
 
+def checkCorporateBond():
+    #회사채 AA( 회사채 AA-(무보증 3년) )의 지수가 떨어지면 매수의 가중치를 준다
+    ratios = []
+    
+    for i in range(1,4) :
+        df = bond.get_otc_treasury_yields(ourTime.ago(-1 * i))
+        ratios.append(df.loc['회사채 AA-(무보증 3년)']['대비'])
+        
+    최대증감율 = max(ratios, key=abs)
+    최소증감율 = min(ratios, key=abs)
+    
+    derivedValue = ( abs( 최대증감율 - 최소증감율 ) / abs(최대증감율) ) * 100 * 0.3
+    
+    if derivedValue > ourConstant.__corporate_bond_standard__ :
+        print("how about wait for right timming")
+    else :
+        print("buy something")
+    
 #hankyong()
 #naver()
 
-checkBond()
+checkCorporateBond()
+checkNationalBond()
 
 #toTextFile.printToText(txts)
 
--- ourTime.py
+++ ourTime.py
@@ -23,28 +23,28 @@ def ago(gap : int):
         raise TypeError("Gap must be integer")
         return
     
-    if gap < -7 or gap == 0 or gap > 7 :
+    if gap < -7 or  gap == 0 or gap > 7 :
         raise IndexError("We can't provide with that gap")
         
     KST = timezone(timedelta(hours=0)) #추후 오레곤 차이로 교체
-    time_record = datetime.now(KST)
-    temp_time_result = time_record + timedelta(days= ( gap ))
+    today = datetime.now(KST)
+    gapDaysAgo = today + timedelta(days= ( gap ))
     
-    if time_record.weekday() < 5 and temp_time_result.weekday() < 5 :
-        return yyyymmdd(temp_time_result)
+    if today.weekday() < 5 and gapDaysAgo.weekday() < 5 :
+        return yyyymmdd(gapDaysAgo)
     
     #time record랑 temp사이 주말 갯수 세기
-    count  = 0
-    gapCount = 0
+    holidayIncludedCount  = 0
+    index = 0
     if gap > 0 :
-        while gapCount < gap :
-            count += 1
-            if getGabWeedDay(time_record, count) < 5:
-                gapCount += 1
+        while index < gap :
+            holidayIncludedCount += 1
+            if getGabWeedDay(today, holidayIncludedCount) < 5:
+                index += 1
     elif gap < 0 :
-        while gap < gapCount :
-            count -= 1
-            if getGabWeedDay(time_record, count) < 5:
-                gapCount -= 1
+        while gap < index :
+            holidayIncludedCount -= 1
+            if getGabWeedDay(today, holidayIncludedCount) < 5:
+                index -= 1
 
-    return yyyymmdd( time_record + timedelta( days = count ))
+    return yyyymmdd( today + timedelta( days = holidayIncludedCount ))
